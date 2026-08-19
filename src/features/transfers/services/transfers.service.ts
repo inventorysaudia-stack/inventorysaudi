ee72bbb488b6b1f1cc0a9edcdd3268d889865891
inventorysaudia-stack <inventorysaudia@gmail.com>
2026-08-20 00:13:38 +0300

    feat(transfers): scaffold transfers service

---
+++ src/features/transfers/services/transfers.service.ts
@@ -0,0 +1,6 @@
+import { transfersRepository } from "../repositories/transfers.repository"
+
+// Service: Business Rules فقط
+export const transfersService = {
+  // TODO: implement in next phase
+}
